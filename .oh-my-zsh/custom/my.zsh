f848c4478f6f319e4d2c684b96bd22a29668f8c8
TheBlueGenie <thebluegenieuk@gmail.com>
2024-01-31 13:09:38 +0000

    Initial Commit

---
+++ .oh-my-zsh/custom/my.zsh
@@ -0,0 +1,9 @@
+export PATH="/home/stu/.local/bin:$PATH"
+
+alias i3c="nvim ~/.config/i3/config"
+alias mzsh="nvim ~/.oh-my-zsh/custom/my.zsh"
+alias pj="cd ~/projects"
+
+alias gc="git clone"
+alias gs="git status"
+alias ga="git add"
