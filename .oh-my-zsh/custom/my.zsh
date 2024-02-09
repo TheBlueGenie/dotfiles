bee1db3e98d2b3ef91d49b46cbe89807d190d128
TheBlueGenie <thebluegenieuk@gmail.com>
2024-02-09 09:47:45 +0000

    change alacritty font and add alias for nvim config

--- .oh-my-zsh/custom/my.zsh
+++ .oh-my-zsh/custom/my.zsh
@@ -3,9 +3,10 @@ export PATH="/home/stu/.local/bin:$PATH"
 alias i3c="nvim ~/.config/i3/config"
 alias mzsh="nvim ~/.oh-my-zsh/custom/my.zsh"
 alias pj="cd ~/projects"
+alias nvconf="cd ~/.config/nvim && nvim"
 
 alias gc="git clone"
 alias gs="git status"
 alias ga="git add"
 
-[ -z "$TMUX"  ] && { tmux attach || exec tmux new-session && exit;}
+#[ -z "$TMUX"  ] && { tmux attach || exec tmux new-session && exit;}
