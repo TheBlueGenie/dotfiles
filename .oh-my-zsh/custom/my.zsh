4d18b44e15a9189673447c88eb0a378134bb658f
TheBlueGenie <thebluegenieuk@gmail.com>
2024-03-01 09:08:40 +0000

    add gruvbox theme to alacritty

--- .oh-my-zsh/custom/my.zsh
+++ .oh-my-zsh/custom/my.zsh
@@ -1,12 +1,15 @@
 export PATH="/home/stu/.local/bin:$PATH"
+export PATH="/home/stu/Odin:$PATH"
 
 alias i3c="nvim ~/.config/i3/config"
 alias mzsh="nvim ~/.oh-my-zsh/custom/my.zsh"
 alias pj="cd ~/projects"
-alias nvconf="cd ~/.config/nvim && nvim"
+alias nvconf="cd ~/.config/nvim && nvim ."
+alias aconf="nvim ~/.config/alacritty.toml"
 
 alias gc="git clone"
 alias gs="git status"
 alias ga="git add"
+alias mgc="git clone git@github.com:TheBlueGenie/$1"
 
 #[ -z "$TMUX"  ] && { tmux attach || exec tmux new-session && exit;}
